d4b285d6a2a957d00c210f9896e56a26b469fd65
shubhamjainkcity <shubhamjainkcity@gmail.com>
2020-05-30 14:16:23 +0530

    chnages done i quiz

--- js/quiz.js
+++ js/quiz.js
@@ -9,7 +9,7 @@ const total = 1;
             let second = time%60;
             if(time<=0)
             {
-                document.getElementById('form1').submit();
+                getScore(document.forms[0]);
             }
             else{
                 if(minute<10)
@@ -25,3 +25,68 @@ const total = 1;
             time--;
 
         }
+
+        var numQues = 5;
+    
+        var numChoi = 3;
+        
+        var answers = new Array(5);
+
+        var scores=   document.getElementById('mark');
+        
+        answers[0] = "Narendra Modi";
+        
+        answers[1] = "Ramnath kovind";
+        
+        answers[2] = "Ashok Gehlot";
+        
+        answers[3] = "delhi";
+        
+        answers[4] = "jaipur";
+    
+        
+        function getScore(form) {
+
+                
+        
+                var score = 0;
+                
+                var curr;
+                
+                var option;
+                
+                for (i=0; i<numQues; i++) {
+                
+                curr = i*numChoi;
+                
+                answered=false;
+                
+                for (j=0; j<numChoi; j++) {
+                
+                option = form.elements[curr + j];
+                
+                if (option.checked) {
+                
+                answered=true;
+                
+                if (option.value == answers[i]) {
+                
+                score++;
+                
+                break;
+                
+                }
+                
+                }
+                
+                }
+                
+                }
+                
+                
+    
+                scores.innerHTML=`Your score is ${score} out of 5`;
+                return false;
+                
+                
+                }
